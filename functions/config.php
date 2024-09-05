8f3ebb3840e8e6cf4059ca3f76f51cd7e5027882
Nova <118440807+nova1987a@users.noreply.github.com>
2024-09-05 15:20:27 +0800

    Update config.php
    Modify for security reason

--- functions/config.php
+++ functions/config.php
@@ -2,15 +2,14 @@
 //Php area is referred from https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php
 // DB connection
 //define('HOST', 'localhost');
-//define('DBUSER', 'egmrdwjs_novatw');
-//define('DBPASS', 'schumann!104');
-//define('DBNAME', 'egmrdwjs_demo1');
-
+//define('DBUSER', 'novatw');
+//define('DBPASS', 'password');
+//define('DBNAME', 'demo1');
+define('DB_PATH', ' $_SERVER['DOCUMENT_ROOT'] . '/dbs/site1.db');
 //Enable connection
 try {
-    $HOME = "/home/balajan/public_html/"
 //    $pdo = new PDO("mysql:host=" . HOST . "; dbname=" . DBNAME, DBUSER, DBPASS);
-    $pdo = new PDO("sqlite:$HOME/site1/dbs/site1.db");
+    $pdo = new PDO("sqlite:" . DB_PATH");
     //Set PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
